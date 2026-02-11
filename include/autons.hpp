8e3690f2ce1fc493c6ec6c348f65a41225c3cd69
Luka8588 <lukas.short@stu.scott.kyschools.us>
2026-02-11 18:31:52 -0500

    Create autons.hpp

---
+++ include/autons.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+void Match_Program_Right();
+void Match_Program_Left();
+void Skills();
+void WorkInProgressSkills();
+void Match_Left_2();
+void Match_Right_2();
+void combining_movements();
+void interfered_example();
+void odom_drive_example();
+void odom_pure_pursuit_example();
+void odom_pure_pursuit_wait_until_example();
+void odom_boomerang_example();
+void odom_boomerang_injected_pure_pursuit_example();
+void measure_offsets();
